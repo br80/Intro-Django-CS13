6d615a1ba531b12a71f5a27a90680efd098af906
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-12-07 12:40:34 -0500

    db settings

--- djorg/settings.py
+++ djorg/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
