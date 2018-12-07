b3af10ff4ec5b02010d7c24b1fd1390697120070
Brady Fukumoto <bradyfukumoto@gmail.com>
2018-12-07 12:43:14 -0500

    Revert

--- djorg/settings.py
+++ djorg/settings.py
@@ -88,8 +88,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
